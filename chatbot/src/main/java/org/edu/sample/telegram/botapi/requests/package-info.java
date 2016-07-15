2c1d7dcb708d9e381abcec55251a69a8595d9452
Vasily Khodyrev <Vasily.Khodyrev@Alcatel-Lucent.com>
2016-07-15 18:17:50 +0300

    telegram

--- chatbot/src/main/java/org/edu/sample/telegram/botapi/requests/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package encapsulates all calls to the Telegram API.
- */
-package org.edu.sample.telegram.botapi.requests;
--- chatbot/src/main/java/org/edu/sample/telegram/botapi/types/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains all types as defined by https://core.telegram.org/bots/api#available-types
- * In addition, a {@link org.edu.sample.telegram.botapi.types.Chat} type is created for convenience.
- */
-package org.edu.sample.telegram.botapi.types;
